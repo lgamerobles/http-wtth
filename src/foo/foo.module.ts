4b4f29fbceeaec8acf77a6ab2fd5ad5b049f3e8b
lgamerobles <lgamerobles@hotmail.com>
2021-04-26 10:24:16 -0500

    ajuste prueba Login http

--- src/foo/foo.module.ts
+++ src/foo/foo.module.ts
@@ -1,6 +1,6 @@
 import { HttpModule, Module } from '@nestjs/common';
-
 import { FooController } from './foo.controller';
+import { Agent } from 'https'
 
 @Module({
     imports: [
@@ -8,7 +8,11 @@ import { FooController } from './foo.controller';
          * Here we can import either the common HttpModule or the one we defined.
          * It doesn't matter as long as our module is at least imported once (for instance in the AppModule)
          */
-        HttpModule,
+         HttpModule.register({
+             httpsAgent: new Agent({
+                 rejectUnauthorized: false
+             })
+         }),
     ],
     controllers: [
         FooController,
